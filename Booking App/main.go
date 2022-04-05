cdd694c1e765c1caf36dcac273ce87a7d6fce997
adore_blvnk <ekhlypse13@gmail.com>
2022-04-05 21:41:58 +0800

    feat(booking): arrays

--- Booking App/main.go
+++ Booking App/main.go
@@ -6,6 +6,7 @@ func main() {
 	conferenceName := "Go Conference" // type inference
 	const conferenceTickets = 50
 	var remainingTickets uint = 50 // declare type
+	var bookings [50]string
 
 	// // print var types
 	// fmt.Printf("conferenceName is %T, conferenceTickets is %T, remainingTickets is %T.\n", conferenceName, conferenceTickets, remainingTickets)
@@ -24,7 +25,11 @@ func main() {
 	fmt.Print("Enter the amount of tickets to purchase: ")
 	fmt.Scan(&userTickets)
 
-	remainingTickets = remainingTickets - userTickets
+	remainingTickets = remainingTickets - userTickets // basic logic
+	bookings[0] = username
+
+	// arrays & slices
+	fmt.Printf("The whole array: %v.\nThe first value: %v.\nArray type: %T.\n Array length: %v.\n", bookings, bookings[0], bookings, len(bookings))
 
 	fmt.Printf("Thank you %v for purchasing %v tickets. You will receive a confirmation email at %v.\n", username, userTickets, email)
 	fmt.Printf("%v tickets remaining for %v.\n", remainingTickets, conferenceName)

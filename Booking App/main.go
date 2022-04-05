3e699348f67f7013dfecd8514b9e2ebfae322aa9
adore_blvnk <ekhlypse13@gmail.com>
2022-04-05 21:13:10 +0800

    feat(booking): data types

--- Booking App/main.go
+++ Booking App/main.go
@@ -3,11 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	var conferenceName string = "Go Conference"
+	conferenceName := "Go Conference" // type inference
 	const conferenceTickets = 50
-	var remainingTickets int = 50
+	var remainingTickets uint = 50 // declare type
 
-	fmt.Printf("Welcome to %s booking application. \nGet your tickets here to attend.\n", conferenceName)
-	fmt.Printf("We have a total of %d tickets and %d tickets left.\n", conferenceTickets, remainingTickets)
+	// print var types
+	fmt.Printf("conferenceName is %T, conferenceTickets is %T, remainingTickets is %T.\n", conferenceName, conferenceTickets, remainingTickets)
+
+	fmt.Printf("Welcome to %v booking application. \n", conferenceName)
+	fmt.Printf("We have a total of %v tickets and %v tickets left.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend.")
 }

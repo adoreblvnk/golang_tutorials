afa181780f4066f64262b5a55db52c455b5d531e
adore_blvnk <ekhlypse13@gmail.com>
2022-04-05 21:47:27 +0800

    feat(booking): slices

--- Booking App/main.go
+++ Booking App/main.go
@@ -6,7 +6,7 @@ func main() {
 	conferenceName := "Go Conference" // type inference
 	const conferenceTickets = 50
 	var remainingTickets uint = 50 // declare type
-	var bookings [50]string
+	bookings := []string{}
 
 	// // print var types
 	// fmt.Printf("conferenceName is %T, conferenceTickets is %T, remainingTickets is %T.\n", conferenceName, conferenceTickets, remainingTickets)
@@ -26,11 +26,12 @@ func main() {
 	fmt.Scan(&userTickets)
 
 	remainingTickets = remainingTickets - userTickets // basic logic
-	bookings[0] = username
+	bookings = append(bookings, username)
 
-	// arrays & slices
-	fmt.Printf("The whole array: %v.\nThe first value: %v.\nArray type: %T.\n Array length: %v.\n", bookings, bookings[0], bookings, len(bookings))
+	// // arrays & slices
+	// fmt.Printf("The whole slice: %v.\nThe first value: %v.\nSlice type: %T.\nSlice length: %v.\n", bookings, bookings[0], bookings, len(bookings))
 
 	fmt.Printf("Thank you %v for purchasing %v tickets. You will receive a confirmation email at %v.\n", username, userTickets, email)
 	fmt.Printf("%v tickets remaining for %v.\n", remainingTickets, conferenceName)
+	fmt.Printf("These are all our bookings: %v.\n", bookings)
 }

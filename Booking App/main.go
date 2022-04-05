16e24faa9b456ab06a6db299b3f29ea7b78fe9a3
adore_blvnk <ekhlypse13@gmail.com>
2022-04-05 21:31:01 +0800

    feat(booking): user input & pointers

--- Booking App/main.go
+++ Booking App/main.go
@@ -7,10 +7,21 @@ func main() {
 	const conferenceTickets = 50
 	var remainingTickets uint = 50 // declare type
 
-	// print var types
-	fmt.Printf("conferenceName is %T, conferenceTickets is %T, remainingTickets is %T.\n", conferenceName, conferenceTickets, remainingTickets)
+	// // print var types
+	// fmt.Printf("conferenceName is %T, conferenceTickets is %T, remainingTickets is %T.\n", conferenceName, conferenceTickets, remainingTickets)
 
 	fmt.Printf("Welcome to %v booking application. \n", conferenceName)
 	fmt.Printf("We have a total of %v tickets and %v tickets left.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend.")
+
+	// user input
+	var username, email string
+	var userTickets int
+	fmt.Print("Enter your username: ")
+	fmt.Scan(&username)
+	fmt.Print("Enter your email: ")
+	fmt.Scan(&email)
+	fmt.Print("Enter the amount of tickets to purchase: ")
+	fmt.Scan(&userTickets)
+	fmt.Printf("Thank you %v for purchasing %v tickets. You will receive a confirmation email at %v.\n", username, userTickets, email)
 }

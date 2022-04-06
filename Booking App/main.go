a00f65e541285698bf84a0a59bd5753e04da0957
adore_blvnk <ekhlypse13@gmail.com>
2022-04-06 14:16:01 +0800

    feat(booking): packages

--- Booking App/main.go
+++ Booking App/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"booking_app/helper"
 	"fmt"
 	"strings"
 )
@@ -24,13 +25,6 @@ func getFirstNames() []string {
 		firstNames = append(firstNames, strings.Fields(booking)[0])
 	}
 	return firstNames
-}
-
-func validateUserInput(firstName string, lastName string, email string, userTickets uint) (bool, bool, bool) {
-	isValidName := len(firstName) >= 2 && len(lastName) >= 2
-	isValidEmail := strings.Contains(email, "@")
-	isValidTicketNumber := userTickets > 0 && userTickets <= remainingTickets
-	return isValidName, isValidEmail, isValidTicketNumber
 }
 
 func getUserInput() (string, string, string, uint) {
@@ -76,7 +70,7 @@ func main() {
 		firstName, lastName, email, userTickets := getUserInput()
 
 		// input validation
-		isValidName, isValidEmail, isValidTicketNumber := validateUserInput(firstName, lastName, email, userTickets)
+		isValidName, isValidEmail, isValidTicketNumber := helper.ValidateUserInput(firstName, lastName, email, userTickets, remainingTickets)
 
 		if isValidName && isValidEmail && isValidTicketNumber {
 
